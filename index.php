74685d3e4fa20eac905079d71e136f9e65d570b4
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-05-13 17:16:29 -0600

    bootstrap's bootstraps

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<meta charset="UTF-8"
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<link rel="stylesheet" href="css/bootstrap.min.css"
 		<title>
 			bootstrap bootstrap bill
 		</title>
 	</head>
 	<body>
-		<p>Bill Turner</p>
+
 	</body>
 </html>
