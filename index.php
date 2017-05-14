d5a911f3606ff27da1d12ae8ca9558649976f56b
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-05-14 16:57:21 -0600

    added meta and boot strap style stuff to hed tag

--- index.php
+++ index.php
@@ -2,14 +2,23 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
+		<!--this is for all the lame people who still use Internet Explorer -->
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta http-equiv="x-ua-compatible" content="ie=edge">
-		<link rel="stylesheet" href="css/bootstrap.min.css"
+		<!--bootstrap stuff latest complied and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<!--Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+		<!-- Font Awesome from the site-->
+		<link type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+		<!-- jQuery v3.0 -->
+		<!--<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js" type="text/javascript"></script> -->
 		<title>
 			bootstrap bootstrap bill
 		</title>
 	</head>
-	<body>
+	<body class="sfooter">
+		<div class="sfooter-content"></div>
 
 	</body>
 </html>
