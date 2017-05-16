88b008322e2aee1301c1b5de034e7529c82389bb
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-05-15 22:11:41 -0600

    added pirate images for bootstrap's bootstraps

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
+
 		<meta charset="UTF-8">
-		<!--this is for all the lame people who still use Internet Explorer -->
+		<!--this is for all the people who still use Internet Explorer -->
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<meta name="viewport" content="width-device-width, initial-scale=1">
 
 		<!--bootstrap stuff latest complied and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
@@ -12,13 +13,32 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 		<!-- Font Awesome from the site-->
 		<link type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+
+		<!-- font awesome -->
+		<link type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+
+		<!-- google font -->
+		<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+
+		<!-- CSS -->
+		<link rel="stylesheet" href="css/style.css" type="text/css"/>
+
 		<!-- jQuery v3.0 -->
-		<!--<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js" type="text/javascript"></script> -->
-		<title>
-			bootstrap bootstrap's
-		</title>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js" type="text/javascript"></script>
+
+
+		<!-- latest complied and minifield JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+
+	</head>
+
 	<body>
 
+<!-- page header -->
+		<div class="page-header">
+			<h1>Giant text of importance<small>next to tiny text of significance</small></h1>
+		</div>
 
 
 
